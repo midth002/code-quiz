1776699d42da5e354300f7e36d3c60afae19313f
Andrew Midthun <midth002@gmail.com>
2022-03-28 09:16:59 -0500

    added timer interval and event listener to the start quiz button.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,24 @@
-alert("Coding Quiz Challenge")
+var startQuizBtn = document.getElementById('start-quiz');
+var timerEl = document.getElementById('time-countdown');
+
+function countdown() {
+    
+
+}
+
+startQuizBtn.addEventListener("click", function() {
+    var timeLeft = 10;
+    
+    var timeInterval = setInterval(function() {
+
+        if (timeLeft >= 1) {
+            timeLeft--;
+            timerEl.textContent = "Time: " + timeLeft;
+        } else {
+            timerEl.textContent = "Time is up!";
+            clearInterval(timeInterval);
+        }
+        
+    }, 1000)
+
+})
