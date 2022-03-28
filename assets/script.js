2a6c197b1f44070311d60b9884bfeb1ae811c3fe
Andrew Midthun <midth002@gmail.com>
2022-03-28 14:52:56 -0500

    made a makeOrderList function to loop the first question I have in the javascript quiz. Only console logging for testing purposes, but was able to get the three indexes to print on the console.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,24 @@
 var startQuizBtn = document.getElementById('start-quiz');
 var timerEl = document.getElementById('time-countdown');
+var quizQuestionEl = document.getElementById('questions');
+var sectionEl = document.getElementById('container');
+var orderListEl = document.getElementById('')
 
-function countdown() {
-    
+var question1 = 'What is the correct syntax for referring to an external script called "xxx.js"';
 
-}
+
+
 
 startQuizBtn.addEventListener("click", function() {
     var timeLeft = 10;
     
-    var timeInterval = setInterval(function() {
+    quizQuestionEl.textContent = question1;
+    console.log(quizQuestionEl);
 
+ 
+   
+    
+    var timeInterval = setInterval(function() {
         if (timeLeft >= 1) {
             timeLeft--;
             timerEl.textContent = "Time: " + timeLeft;
@@ -21,4 +29,23 @@ startQuizBtn.addEventListener("click", function() {
         
     }, 1000)
 
+    makeOrderList();
+
 })
+
+
+function  makeOrderList() {
+    var multipleChoice1 = ['<script name="xxx.js">', '<script href="xxx.js"?', '<script src="xxx.js">'];
+
+    for (var i = 0; i < multipleChoice1.length; i++) {
+        var item = document.createElement('li');
+        item.textContent = multipleChoice1[i];
+        console.log(item);
+    }
+    
+    return orderListEl;
+
+}
+
+
+
