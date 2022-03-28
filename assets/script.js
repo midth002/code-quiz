ba565e1d43ef420dd3a673b13cda03e5f58cce8e
Andrew Midthun <midth002@gmail.com>
2022-03-28 09:03:46 -0500

    Added initial start screen in the html page.

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-// pseudo coded 
-
-// HTML 
-    // 1. Start screen title, rules, start button, timer
-    // 2. Question screen container with question text, buttons, timer
-    // 3. Show final score. Input for entering your initials, and submit button. 
-    // High Scores html page. 
-
-// 
